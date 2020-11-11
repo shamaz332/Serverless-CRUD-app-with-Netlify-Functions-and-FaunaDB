8b1998a96ae31d0a65af36b7d35341ac8e953bb4
shamaz332 <shamazsaeed332@gmail.com>
2020-11-11 23:35:02 +0500

    del error

---
+++ functions/deleteTodo.js
@@ -0,0 +1,23 @@
+const faunadb = require("faunadb")
+q = faunadb.query
+const handler = async event => {
+  try {
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" }
+    }
+    const client = new faunadb.Client({ secret: 'fnAD6M-o9uACB4lYdz8SDfauG0rT2BnCDJSx0GjY'})
+    const Item = JSON.parse(event.body)
+    const result = await client.query(
+      q.Delete(q.Ref(q.Collection("products"), Item.id))
+    )
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Item Deleted` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
--- src/todos/readTodo.tsx
+++ src/todos/readTodo.tsx
@@ -7,7 +7,7 @@ export const ReadTodo = () => {
     const [dataa, setData] = useState([])
 
     useEffect(() => {
-         (async () => {
+        (async () => {
             await fetch("/.netlify/functions/readTodo")
                 .then(res => res.json())
 
@@ -18,19 +18,31 @@ export const ReadTodo = () => {
         })()
     }, [])
 
+    const deleteMessage = async message => {
+
+        await fetch("/.netlify/functions/deleteTodo", {
+            method: "post",
+            body: JSON.stringify({ id: message.ref["@ref"].id }),
+        })
+
+    }
     return (
         <div>
 
-            {dataa === null || dataa === undefined ? <div>leading</div>:
-<div>
-            {dataa.map((mes, i) => (
-                <div key={i}>
-                    <p>{mes.data.detail}</p>
+            {dataa === null || dataa === undefined ? <div>leading</div> :
+                <div>
+                    {dataa.map((mes, i) => (
+                        <div key={i}>
+                            <p>{mes.data.detail}</p>
+                            <button onClick={() => {
+                                deleteMessage(mes)
+                            }}> Delete</button>
+                        </div>
+
+                    ))}
                 </div>
-            ))}
-             </div>
             }
-            
+
         </div>
     )
 }
